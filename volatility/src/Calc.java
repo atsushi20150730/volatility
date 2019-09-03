0faeb8718603daa9156e9583652db967474f646b
atsushi20150730 <atsushi.20140801@gmail.com>
2019-09-03 16:20:12 +0900

    Update Calc.java

--- volatility/src/Calc.java
+++ volatility/src/Calc.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.math.BigDecimal;
 import java.util.ResourceBundle;
 
@@ -8,6 +9,7 @@ public class Calc {
     private static ResourceBundle target = ResourceBundle.getBundle("target");
     private static ResourceBundle rate = ResourceBundle.getBundle("rate");
     private static ResourceBundle open = ResourceBundle.getBundle("open");
+    private static StringBuilder sb = new StringBuilder();
 
     private static void exe() throws Exception {
         // currency
@@ -37,24 +39,28 @@ public class Calc {
         BigDecimal work2 = Util.B1.divide(median, 10, BigDecimal.ROUND_HALF_UP);
         BigDecimal lots = amount.multiply(work1);
         lots = lots.multiply(work2).multiply(Util.B100);
-        System.out.println(currency + " : " + String.format("%,d", lots.intValue()));
-        System.out.println("median : " + medianLimit + Util.LS + Util.LS);
+        println(currency + " : " + lots.intValue());
+        println("median : " + medianLimit + Util.LS + Util.LS);
 
         // buy
         BigDecimal startBuy = openPrice.subtract(medianStart);
         BigDecimal limitBuy = startBuy.add(medianLimit);
         BigDecimal stopBuy = startBuy.subtract(medianLimit);
-        System.out.println("Buy  : " + startBuy);
-        System.out.println("       " + limitBuy);
-        System.out.println("       " + stopBuy + Util.LS + Util.LS);
+        println("Buy  : " + startBuy);
+        println("       " + limitBuy);
+        println("       " + stopBuy + Util.LS + Util.LS);
 
         // sell
         BigDecimal startSell = openPrice.add(medianStart);
         BigDecimal limitSell = startSell.subtract(medianLimit);
         BigDecimal stopSell = startSell.add(medianLimit);
-        System.out.println("Sell : " + startSell);
-        System.out.println("       " + limitSell);
-        System.out.println("       " + stopSell);
+        println("Sell : " + startSell);
+        println("       " + limitSell);
+        println("       " + stopSell);
+    }
+
+    private static void println(String val) {
+        sb.append(val + Util.LS);
     }
 
     public static void main(String[] args) throws Exception {
@@ -64,5 +70,10 @@ public class Calc {
             targetIn = new File(System.getProperty("user.dir") + "/src/all");
         }
         exe();
+
+        File output = new File(System.getProperty("user.home"), "output.txt");
+        FileWriter fw = new FileWriter(output);
+        fw.write(sb.toString());
+        fw.close();
     }
 }
